26468010cd046f9f8067946a75e6703bbefbbd31
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-20 22:01:28 -0300

    add database connection settings

---
+++ src/database/index.ts
@@ -0,0 +1,14 @@
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (): Promise<Connection> => {
+    // get info from ormconfig.json file
+    const defaultOptions = await getConnectionOptions();
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database:
+                process.env.NODE_ENV === 'test'
+                    ? ""
+                    : defaultOptions.database
+        })
+    );
+}
