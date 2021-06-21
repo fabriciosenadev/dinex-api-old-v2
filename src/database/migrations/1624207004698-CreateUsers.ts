1452721b4b44aff604238c33068fab38176ac470
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-21 00:07:23 -0300

    add users migration

---
+++ src/database/migrations/1624207004698-CreateUsers.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1624207004698 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "full_name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "password",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_on",
+                        type: "timestamp",
+                        isNullable: true
+                    },
+                    {
+                        name: "updated_on",
+                        type: "timestamp",
+                        isNullable: true
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
