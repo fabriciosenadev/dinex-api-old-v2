1bbaad0dea003a2b49ff0cd4afbc820c734df09d
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-21 00:03:52 -0300

    add database connection

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import 'reflect-metadata'; // used by database
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-
-//TODO: need to be implemented to use database
-// import createConnection from './database';
-
+import createConnection from './database';
 import { AppError } from './errors/AppError';
 import { appRouter } from './routes';
 
+createConnection();
 // instance of all express app
 const app = express();
 
@@ -25,6 +23,7 @@ app.use((err: Error, request: Request, response: Response, _next: NextFunction) 
         });
     }
 
+    // if any non mapped error returns
     return response.status(500).json({
         status: "Error",
         message: `Internal server error ${err.message}`,
