f6dee3de0640128ff6780b5224807b72018b3d50
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-21 00:04:45 -0300

    fix model to work with database properly

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,11 @@
 import { Column, Entity, PrimaryColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity("user")
+@Entity("users")
 class User {
+    //#region Props
     @PrimaryColumn()
-    readonly id: string;
+    private readonly id: string;
 
     @Column()
     full_name: string;
@@ -15,11 +16,58 @@ class User {
     @Column()
     password: string;
 
+    @CreateDateColumn()
+    created_on: Date;
+
+    @UpdateDateColumn({
+        type: "datetime",
+        nullable: true
+    })
+    updated_on: Date;
+    //#endregion
+
     constructor() {
         if (!this.id) {
             this.id = uuid();
         }
     }
+
+    //#region Encapsuling
+    // public get fullName() {
+    //     return this.full_name;
+    // }
+    // public set fullName(fullName: string) {
+    //     this.full_name = fullName;
+    // }
+
+    // public get emailAddress() {
+    //     return this.email;
+    // }
+    // public set emailAddress(emailAddress: string) {
+    //     this.email = emailAddress;
+    // }
+
+    // public get pass() {
+    //     return this.password;
+    // }
+    // public set pass(pass: string) {
+    //     this.password = pass;
+    // }
+
+    // public get createdOn() {
+    //     return this.created_on;
+    // }
+    // public set createdOn(createdOn: Date) {
+    //     this.created_on = createdOn;
+    // }
+    
+    // public get updatedOn() {
+    //     return this.updated_on;
+    // }
+    // public set updatedOn(updatedOn: Date) {
+    //     this.updated_on = updatedOn;
+    // }
+    //#endregion
 }
 
 export { User };
