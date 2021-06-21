1d3985d97cd165f8681fcc25090630491e73a8b6
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-20 22:00:48 -0300

    add to utils a cryptographer class

---
+++ src/utils/Cryptography.ts
@@ -0,0 +1,33 @@
+import * as crypto from "crypto-js";
+import { AppError } from "../errors/AppError";
+
+class Cryptography{
+    public static async doEncrypt(string: string) {
+        try {
+            const encrypted = crypto.AES.encrypt(
+                string.trim(),
+                process.env.PASS_KEY
+            ).toString()
+
+            return encrypted;
+        } catch (error) {
+            throw new AppError("Error to encrypt - " + error);
+        }
+    }
+    public static async doDecrypt(string: string){
+        try {
+            const decrypted = crypto.AES.decrypt(
+                string.trim(),
+                process.env.PASS_KEY
+            ).toString()
+
+            return decrypted;
+        } catch (error) {
+            console.log("error:"+error);
+            
+            throw new AppError("Error to decrypt" + error);
+        }
+    }
+}
+
+export { Cryptography };
