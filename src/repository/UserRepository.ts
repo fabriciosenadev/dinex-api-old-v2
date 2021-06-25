bf0ed927af64906acfbb7dbad859a236c7b3bbd6
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-25 00:31:22 -0300

    improve code

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -2,8 +2,6 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from "../models/User";
 
 @EntityRepository(User)
-class UserRepository extends Repository<User> {
+export class UserRepository extends Repository<User> {
 
 }
-
-export { UserRepository };
