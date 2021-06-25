fd01dd9200baeeaad224e29f4965e098a0e5d428
Fabricio Sena <fabricior.sena@outlook.com>
2021-06-25 00:32:32 -0300

    add class to relation category and user

---
+++ src/services/CategoryUserService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/AppError";
+import { IsCustom } from "../models/enums/IsCustom";
+import { CategoryRepository } from "../repository/CategoryRepository";
+import { CategoryUserRepository } from "../repository/CategoryUserRepository";
+
+export class CategoryUserService {
+    public static async addStandardCategoriesToUser(userId: string) {
+        try {
+            const categoryUserRepository = getCustomRepository(CategoryUserRepository);
+            const categoryRepository = getCustomRepository(CategoryRepository);
+
+            let arrCategories = await categoryRepository.find({
+                is_custom: Number(IsCustom.No)
+            });
+            arrCategories.forEach(async (category) => {
+                const newCategoryUser = await categoryUserRepository.create({
+                    category_id: category.id,
+                    user_id: userId,
+                });
+                console.log(newCategoryUser);
+                
+                await categoryUserRepository.save(newCategoryUser);
+            });
+        }
+        catch (error) {
+            console.log(error);
+            throw new AppError(error);
+        }
+
+    }
+}
