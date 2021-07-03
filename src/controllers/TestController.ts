9d86f4fe7356eca226fe4d0649759f61b4c9c1e0
Fabricio Sena <fabricior.sena@outlook.com>
2021-07-03 00:49:18 -0300

    add new feature to TestController

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { AppError } from "../errors/AppError";
+import { Cryptography } from '../utils/Cryptography';
 
 /**  FIRST TEST
  * basic structure to be implemented on  any other controller
@@ -11,6 +12,18 @@ class TestController {
             response.json("kkk");
         } catch (error) {
             throw new AppError("first error got");
+        }
+    }
+    async encrypt(request: Request, response: Response) {
+        try {
+            const { pass1, pass2 } = request.body;
+
+            const encryptPass1 = await Cryptography.doEncrypt(pass1);
+            const encryptPass2 = await Cryptography.doEncrypt(pass2);
+
+            response.json({ encryptPass1, encryptPass2 });
+        } catch (err) {
+            throw new AppError(err);
         }
     }
 }
