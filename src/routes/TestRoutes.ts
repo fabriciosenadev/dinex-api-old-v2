9a1db4d4f41dfe8f184cb44678bf99db62a4e98f
Fabricio Sena <fabricior.sena@outlook.com>
2021-07-03 00:50:00 -0300

    improve test routes by env

--- src/routes/TestRoutes.ts
+++ src/routes/TestRoutes.ts
@@ -10,6 +10,9 @@ const testController = new TestController();
 // implemented routes using prefix /test
 const testRoutes = Router();
 
-testRoutes.get('/', testController.firstGet);
+if (process.env.NODE_ENV === 'dev') {
+    testRoutes.get('/', testController.firstGet);
+    testRoutes.post('/encrypt', testController.encrypt); // route to be used in dev
+}
 
 export { testRoutes };
