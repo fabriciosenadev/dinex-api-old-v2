7d73ba8bb397582528c97bb94271d9eecd456c36
Fabricio Sena <fabricior.sena@outlook.com>
2021-07-05 23:10:06 -0300

    allowed new prefix to categories routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,16 @@ import express from 'express';
 //import here all individual routes
 import { testRoutes } from './routes/TestRoutes';
 import { userRoutes } from './routes/UserRoutes';
+import { categoryRoutes } from './routes/CategoryRoutes';
 
 // all routes to be used by app
 const appRouter = express();
 
 // implemented routes by prefix
 appRouter.use('/test', testRoutes);
-appRouter.use('/user', userRoutes);
-// appRouter.use('/launch', launchRoutes);
-// appRouter.use('/category', categoryRoutes);
+appRouter.use('/users', userRoutes);
+appRouter.use('/categories', categoryRoutes);
+// appRouter.use('/launches', launchRoutes);
 // appRouter.use('/system', systemRoutes);
 
 export { appRouter };
