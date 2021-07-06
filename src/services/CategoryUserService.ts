5bc77b645aba9dbaac0bb85c77696d4f46421120
Fabricio Sena <fabricior.sena@outlook.com>
2021-07-05 23:16:51 -0300

    add method to create relation and code improves

--- src/services/CategoryUserService.ts
+++ src/services/CategoryUserService.ts
@@ -7,8 +7,8 @@ import { CategoryUserRepository } from "../repository/CategoryUserRepository";
 export class CategoryUserService {
     public static async addStandardCategoriesToUser(userId: string) {
         try {
-            const categoryUserRepository = getCustomRepository(CategoryUserRepository);
-            const categoryRepository = getCustomRepository(CategoryRepository);
+            const categoryUserRepository = await this.categoryUserRepository();
+            const categoryRepository = await this.categoryRepository();
 
             let arrCategories = await categoryRepository.find({
                 is_custom: Number(IsCustom.No)
@@ -17,14 +17,38 @@ export class CategoryUserService {
                 const newCategoryUser = await categoryUserRepository.create({
                     category_id: category.id,
                     user_id: userId,
-                });                
+                });
                 await categoryUserRepository.save(newCategoryUser);
             });
         }
         catch (error) {
             console.log(error);
-            throw new AppError(error);
+            throw new AppError("Ocorreu um erro interno: " + error, 500);
         }
 
+    }
+
+    public static async addCategoryRelationToUser(categoryId: number, userId: string) {
+        try {
+            const categoryUserRepository = await this.categoryUserRepository();
+            const newRelation = await categoryUserRepository.create({
+                category_id: categoryId,
+                user_id: userId
+            })
+            console.log(newRelation);
+
+            await categoryUserRepository.save(newRelation);
+        } catch (error) {
+            console.log(error);
+            throw new AppError("Ocorreu um erro interno: " + error, 500);
+        }
+    }
+
+    private static async categoryUserRepository() {
+        return getCustomRepository(CategoryUserRepository);
+    }
+
+    private static async categoryRepository() {
+        return getCustomRepository(CategoryRepository);
     }
 }
