cada5d435eaa2d7e2cc6e7418097d4b99ded91ef
Fabricio Sena <fabricior.sena@outlook.com>
2021-07-18 16:46:33 -0300

    change token validation

--- src/utils/WebToken.ts
+++ src/utils/WebToken.ts
@@ -3,13 +3,14 @@ import { AppError } from "../errors/AppError";
 
 export class WebToken {
     public static async decodeToUserId(token: any) {
+        await this.verifyAuthorization(token);
         return jwt.verify(
             token,
             process.env.SECRET,
             (error, decoded) => {
                 if (error)
                     throw new AppError(error, 500);
-                
+
                 return decoded.id;
             });
     }
@@ -18,5 +19,10 @@ export class WebToken {
         return jwt.sign({ id }, process.env.SECRET, {
             expiresIn: 3600 * 24 // expires in a day
         });
+    }
+
+    private static async verifyAuthorization(token: string) {
+        if (!token)
+            throw new AppError("No token provided", 401);
     }
 }
